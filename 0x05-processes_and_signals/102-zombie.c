0a48234ec936d453e0f22c3350472a17dd180ec0
minanadynarouz <mina.n.narouz@gmail.com>
2023-12-24 14:11:22 -0800

    task 102

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - loop runs infinitly
+ * Return: zero if broken
+ */
+
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+/**
+ * main - program that creates 5 zombies processes
+ * Return: 0 if success, 1 otherwise
+ */
+
+int main(void)
+{
+	pid_t childs_pid;
+	int counter = 0;
+
+	while (counter < 5)
+	{
+		if (childs_pid > 0)
+		{
+			childs_pid = fork();
+			printf("Zombie process created, PID: %d\n", childs_pid);
+			sleep(1);
+			counter++;
+		}
+		else
+		{
+			exit(0);
+		}
+	}
+	infinite_while();
+	return (0);
+}
