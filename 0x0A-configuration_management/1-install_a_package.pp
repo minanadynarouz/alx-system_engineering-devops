b8f17aa338401b31f1d7a1394966e20cb37e9a08
Mina Nady Narouz <131834381+minanadynarouz@users.noreply.github.com>
2024-02-25 13:39:48 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Install Flask version 2.1.0 using pip3
-
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install python packages':
+  command => 'pip3 install flask==2.1.0',
+  path    => ['/usr/bin/'],
+  unless  => '/usr/bin/test -f /usr/local/lib/python3.8/dist-packages/flask/app.py',
 }
