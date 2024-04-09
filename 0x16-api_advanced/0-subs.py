dc9e3cf8f388279af8f20f3d345b20d03d88b196
minanadynarouz <mina.n.narouz@gmail.com>
2024-04-09 22:59:06 +0000

    a

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,27 @@
-import requests
+#!/usr/bin/python3
 """
 module contains function to get no. of reddit's
 subreddit subscribers.
 """
 
+import requests
+
 
 def number_of_subscribers(subreddit):
     """
-    Query the Reddit API and fetch data about a subreddit's subscribers.
+    query the reddit API, and fetch data about a
+    reddit's subreddit
     """
 
     headers = {
-        "Accept": "*/*",
-        "User-Agent": "Student script",
+            "Accept": "*/*",
+            "User-Agent": "ALX student script",
     }
-
     api_url = 'https://www.reddit.com/r'
     url = '{}/{}/about.json?raw_json=1'.format(api_url, subreddit)
-    res = requests.get(url, headers=headers, allow_redirects=False)
+    res = requests.get(url, headers=headers)
     if not res.ok:
         return 0
     data = res.json().get("data")
     subscribers = data.get("subscribers")
     return subscribers
-
