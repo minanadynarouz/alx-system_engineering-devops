371cd91a0c3c6ff53f7643808c1b46e9ab1415ec
minanadynarouz <mina.n.narouz@gmail.com>
2024-04-09 22:38:06 +0000

    Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Query a subreddit and return the number of
+total subscribers in that subredit
+"""
+
+from requests import get
+from sys import argv
+
+
+headers = {
+    "User-Agent": "Of course custom User-Agent",
+    "X-Forwared-For": "iamthecoder"
+}
+
+
+def number_of_subscribers(subreddit):
+    """
+    Query the subreddit and return the number of
+    Active subs. If its an invalid subredit, return 0
+    """
+    response = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
+                   headers=headers)
+    data = response.json()
+
+    try:
+        if 'error' in data.keys():
+            return 0
+        else:
+            return data['data']['subscribers']
+    except Exception as e:
+        return 0
+
+if __name__ == '__main__':
+    print(number_of_subscribers(argv[1]))
