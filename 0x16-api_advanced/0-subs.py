e0ade8c080b700b786b3d1582961534c8579428f
minanadynarouz <mina.n.narouz@gmail.com>
2024-04-09 22:44:34 +0000

    Amend

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python3
+
 """
-This module contains function that queries the Reddit API
+Query a subreddit and return the number of
+total subscribers in that subredit
 """
-import requests
+
+from requests import get
+from sys import argv
 
 
-def number_of_subscribers(subreddit):
-    """ Returns the number of subscribers for a given subreddit. """
-    u_agent = 'Mozilla/5.0'
+headers = {
+    "User-Agent": "mozzila 5.0",
+    "X-Forwared-For": "mina"
+}
 
-    headers = {
-        'User-Agent': u_agent
-    }
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
+def number_of_subscribers(subreddit: str) -> int:
+    """
+    Query the subreddit and return the number of
+    Active subs. If its an invalid subredit, return 0
+    """
+    response = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
+                   headers=headers)
+    data = response.json()
+    try:
+        if 'error' in data.keys():
+            return 0
+        else:
+            return data['data']['subscribers']
+    except Exception as e:
         return 0
-    dic = res.json()
-    if 'data' not in dic:
-        return 0
-    if 'subscribers' not in dic.get('data'):
-        return 0
-    return res.json()['data']['subscribers']
+
+
+if __name__ == "__main__":
+    print(number_of_subscribers(argv[1]))
